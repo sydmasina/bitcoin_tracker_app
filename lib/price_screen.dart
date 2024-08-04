615b4e8ae32b0ece912512fa8e30b313b26d5aef
sydmasina <masinasydwell@gmail.com>
2024-08-04 10:11:20 +0200

    refactor to use different dropdowns for different os systems

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,5 +1,7 @@
 import 'package:bitcoin_tracker_app/constants/coin_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -8,6 +10,41 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String dropdownValue = currenciesList.first;
+
+  DropdownButton androidPicker() {
+    return DropdownButton(
+      value: dropdownValue,
+      items: currenciesList.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      onChanged: (value) {
+        setState(() {
+          if (value != null) {
+            dropdownValue = value;
+          }
+        });
+      },
+    );
+  }
+
+  CupertinoPicker iosPicker() {
+    return CupertinoPicker(
+      itemExtent: 40,
+      onSelectedItemChanged: (int selectedItemIndex) {
+        setState(() {
+          dropdownValue = currenciesList[selectedItemIndex];
+        });
+      },
+      children: List.generate(currenciesList.length, (int index) {
+        return Center(
+          child: Text(currenciesList[index]),
+        );
+      }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,21 +82,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: DropdownButton(
-              value: dropdownValue,
-              items:
-                  currenciesList.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                    value: value, child: Text(value));
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  if (value != null) {
-                    dropdownValue = value;
-                  }
-                });
-              },
-            ),
+            child: Platform.isIOS ? iosPicker() : androidPicker(),
           ),
         ],
       ),
